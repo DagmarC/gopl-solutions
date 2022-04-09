53e1b8a7f846032fec5c967910f56dc1d62dc26a
DagmarMac <dg,cervenkova@gmail.com>
2022-04-09 12:58:41 +0200

    child added

--- ch5/5.17/tagname.go
+++ ch5/5.17/tagname.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/DagmarC/gopl-solutions/ch5"
 	"golang.org/x/net/html"
@@ -20,21 +22,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ElementsByTagName(doc, "h1", "h2", "head")
+	_ = ElementsByTagName(doc,  "title", "h1", "a")
+
 }
 
 func ElementsByTagName(doc *html.Node, findNodes ...string) []*html.Node {
 
 	var result []*html.Node
-
+	fmt.Println(findNodes)
 	nodes := make(map[string]bool, len(findNodes))
 	for _, node := range findNodes {
 		nodes[node] = true
 	}
 
 	pre := func(n *html.Node) {
-		if n.Type == html.ElementNode && nodes[n.Data] {
-			result = append(result, n)
+		if n.Type == html.ElementNode && nodes[n.Data] && n.FirstChild != nil && strings.TrimSpace(n.FirstChild.Data) != "" {
+			result = append(result, n.FirstChild)
+			fmt.Println(n.Data, n.FirstChild.Data)
 		}
 	}
 	ch5.ForEachNode(doc, pre, nil)
