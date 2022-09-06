dfde6fbd308264916bee34fb73d587fc884ea796
DagmarMac <dg,cervenkova@gmail.com>
2022-09-06 14:27:52 +0200

    7.17 change?

--- ch7/7.17/main.go
+++ ch7/7.17/main.go
@@ -9,40 +9,6 @@ import (
 	myxml "github.com/DagmarC/gopl-solutions/ch7/7.17/xml"
 )
 
-// func main() {
-// 	attMatchers := []string{"id", "class"}
-// 	dec := xml.NewDecoder(os.Stdin)
-// 	var stack []string // stack of element names
-
-// 	for {
-// 		tok, err := dec.Token()
-// 		if err == io.EOF {
-// 			break
-// 		} else if err != nil {
-// 			fmt.Fprintf(os.Stderr, "xmlselect: %v\n", err)
-// 			os.Exit(1)
-// 		}
-// 		switch tok := tok.(type) {
-// 		case xml.StartElement:
-// 			stack = append(stack, tok.Name.Local) // push
-// 		case xml.EndElement:
-// 			stack = stack[:len(stack)-1] // pop
-
-// 		case xml.CharData:
-// 			if myxml.ContainsAll(stack, os.Args[1:]) {
-// 				if myxml.ContainsAll(stack, os.Args[1:]) {
-// 					// for _, s := range stack {
-// 					// 	if !strings.Contains(s, "=") {
-// 					// 		fmt.Printf("%s ", s)
-// 					// 	}
-// 					// }
-// 					fmt.Printf(" %s\n", tok)
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
 func main() {
 	dec := xml.NewDecoder(os.Stdin)
 	var stack []xml.StartElement // stack of element names
@@ -58,7 +24,7 @@ func main() {
 		switch tok := tok.(type) {
 		case xml.StartElement:
 			stack = append(stack, tok) // push
-			
+
 		case xml.EndElement:
 			stack = stack[:len(stack)-1] // pop
 
