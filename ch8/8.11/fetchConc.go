f220de2f52010df43c436abe10987825a3f2702b
DagmarMac <dg,cervenkova@gmail.com>
2022-12-19 14:12:47 +0100

    8.11

--- ch8/8.11/fetchConc.go
+++ ch8/8.11/fetchConc.go
@@ -4,7 +4,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -15,7 +14,7 @@ func main() {
 	if len(os.Args[1:]) == 0 {
 		return
 	}
-	fmt.Println(len(os.Args[1:]))
+	fmt.Println("Number of addresses to be fetched: ", len(os.Args[1:]))
 	ch := make(chan string, len(os.Args[1:])) // bufferred chan to prevent the goroutine leak
 	for _, url := range os.Args[1:] {
 		go fetch(url, ch) // start a goroutine
@@ -33,7 +32,7 @@ func fetch(url string, ch chan<- string) {
 		return
 	}
 
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	nbytes, err := io.Copy(io.Discard, resp.Body)
 	resp.Body.Close() // don't leak resources
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
